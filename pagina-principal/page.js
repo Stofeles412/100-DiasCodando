e146a6e115c6bd4c220a36f950547aa766bddf33
Stofeles412 <fernandostofeles@gmail.com>
2024-07-08 12:43:35 -0300

    .

--- pagina-principal/page.js
+++ pagina-principal/page.js
@@ -15,4 +15,18 @@ function openMenu(){
     }
   
 }
+
+var zoom =  false;
+
+function ampliarImg(){
+    var zoom = document.getElementById("main-img")
+    if(zoom.style.width == '90%'){
+        zoom.style.width = '110%'
+        
+    }else{
+    zoom.style.width = '90%'
+   
+
+    }
+}
 confirm ("você aceita cookies ?")
