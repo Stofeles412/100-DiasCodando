98c4b137f9e227be63403033f0afad670bc9f125
Stofeles412 <fernandostofeles@gmail.com>
2024-07-12 19:26:14 -0300

    botei interatividade no icone do menu

--- animes/anime.js
+++ animes/anime.js
@@ -3,11 +3,14 @@ function openMenu(){
     var icone = document.getElementById("icone-menu")
     if(abrir.style.display == 'block'){
         abrir.style.display = 'none'
-        icone.src ('sinal-de-seta-para-baixo-para-navegar.png')
+        icone.src = ('../imagens/sinal-de-seta-para-baixo-para-navegar.png')
+      
        
     }else{
         abrir.style.display = 'block'
-        icone.src ('pra-cima.png')
+        icone.src = ('../imagens/pra-cima.png')
+
+      
     }
 }
 confirm ("ATENÇÃO: ESSE ARTIGO PODE CONTER SPOILER DO ANIME !!")
--- pagina-principal/page.js
+++ pagina-principal/page.js
@@ -5,13 +5,13 @@ function openMenu(){
     if(abrir.style.display == "block"){
         abrir.style.display = "none"
 
-        icone.src ('sinal-de-seta-para-baixo-para-navegar.png')
+        icone.src = ('../imagens/sinal-de-seta-para-baixo-para-navegar.png')
         
          
        
     }else{
         abrir.style.display = "block"
-        icone.src ('pra-cima.png')
+        icone.src = ('../imagens/pra-cima.png')
        
     }
   
