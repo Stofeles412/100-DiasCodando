6ec7ee9ab60ba43d775ce996c7a6692ddc7b5514
Stofeles412 <fernandostofeles@gmail.com>
2024-07-12 13:11:01 -0300

    .

--- animes/anime.js
+++ animes/anime.js
@@ -3,9 +3,11 @@ function openMenu(){
     var icone = document.getElementById("icone-menu")
     if(abrir.style.display == 'block'){
         abrir.style.display = 'none'
+        icone.src ('sinal-de-seta-para-baixo-para-navegar.png')
        
     }else{
         abrir.style.display = 'block'
+        icone.src ('pra-cima.png')
     }
 }
 confirm ("ATENÇÃO: ESSE ARTIGO PODE CONTER SPOILER DO ANIME !!")
--- pagina-principal/page.js
+++ pagina-principal/page.js
@@ -4,12 +4,14 @@ function openMenu(){
 
     if(abrir.style.display == "block"){
         abrir.style.display = "none"
+
+        icone.src ('sinal-de-seta-para-baixo-para-navegar.png')
         
          
        
     }else{
         abrir.style.display = "block"
-      
+        icone.src ('pra-cima.png')
        
     }
   
