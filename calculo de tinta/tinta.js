ce46b82d620afd19d3039b2b4be3c8e305963929
Stofeles412 <fernandostofeles@gmail.com>
2024-08-13 19:15:23 -0300

    .

---
+++ calculo de tinta/tinta.js
@@ -0,0 +1,10 @@
+function openMenu(){
+    var abrir = document.getElementById("nav-itens")
+    var icone = document.getElementById("nav-img")
+    if(abrir.style.display == "none"){
+        abrir.style.display = "block"
+    }else{
+        abrir.style.display = "none"
+    }
+}
+
