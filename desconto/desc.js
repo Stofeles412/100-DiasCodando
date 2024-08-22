c367f5c1a7d60b0b52e558565b8c340ef04dd72c
Stofeles412 <fernandostofeles@gmail.com>
2024-08-22 13:16:31 -0300

    .

--- desconto/desc.js
+++ desconto/desc.js
@@ -1,24 +1,29 @@
 function desconto(){
 
 
-    var valor = parseFloat(document.getElementById("valor").value, 10);
+    var valor = parseFloat(document.getElementById("valor").value);
+    var desconto = parseInt(document.getElementById("des").value)
 
 
 
-des5 = valor * 5/100
+/*des5 = valor * 5/100
 des10 = valor * 10/100
 des20 = valor * 20/100
 valorFinal5 = valor - des5
 valorFinal10 = valor - des10
-valorFinal20 = valor - des20
+valorFinal20 = valor - des20*/
+
+vDes = (valor * desconto) / 100
+var vFinal = valor - desconto
 
 
-res = document.getElementById("res").textContent = ` o produto que antes custava ${valor} com 5% ${des5}  - R$ Reais de desconto o produto ficará por ${valorFinal5} R$`
 
-res2 = document.getElementById("res2").textContent = ` o produto que antes custava ${valor} com 10%  ${des10}R$ Reais de desconto o produto ficará por ${valorFinal10} R$`
+res = document.getElementById("res").textContent = `O produto que antes custava com o des de ${vDes} passou a custar ${vFinal}`
+
+/*res2 = document.getElementById("res2").textContent = ` o produto que antes custava ${valor} com 10%  ${des10}R$ Reais de desconto o produto ficará por ${valorFinal10} R$`*/
 
 
-res3 = document.getElementById("res3").textContent = ` o produto que antes custava ${valor} com 20% - R$ Reais ${des20} de desconto o produto ficará por ${valorFinal20} R$`
+/*res3 = document.getElementById("res3").textContent = ` o produto que antes custava ${valor} com 20% - R$ Reais ${des20} de desconto o produto ficará por ${valorFinal20} R$`*/
 
 
 
