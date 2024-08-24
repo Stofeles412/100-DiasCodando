05fd0f8c28e401a535c77ef4a1617b74394bbed1
Stofeles412 <fernandostofeles@gmail.com>
2024-08-24 14:39:24 -0300

    .

--- desconto/desc.js
+++ desconto/desc.js
@@ -4,14 +4,16 @@ function desconto(){
     var valor = parseFloat(document.getElementById("valor").value);
     var desconto = parseFloat(document.getElementById("des").value)
 
+  
     var desc2 = desconto.toFixed(2)
 
     var vDes = (valor * desconto) / 100;
-var vFinal = valor - vDes;
+    var vFinal = valor - vDes;
 
 
 
-res = document.getElementById("res").textContent = `O produto que custava ${valor.toFixed(2)}R$ com  ${vDes}% de desconto, fica por ${vFinal.toFixed(2)}R$`
+res = document.getElementById("res").textContent = `O produto que custava ${valor.toFixed(2)}R$ com  ${vDes.toFixed(1)}% de desconto, fica por ${vFinal.toFixed(2)}R$`
+
 
  
 
