50218cd81f97cad5546da79213912126d3599753
Stofeles412 <fernandostofeles@gmail.com>
2024-09-19 20:49:20 -0300

    Update index.py

--- curso-python/index.py
+++ curso-python/index.py
@@ -1,7 +1,6 @@
-nome = input("como se chama ?")
-ano = 2024
-nascimento = int(input("qual ano voce nasceu ?"))
-idade = ano - nascimento
+nome = input("Digite seu nome de usuário: ")
 
-print("seja bem vindo(a) {}".format(nome))
-print("já que você nasceu no ano de {} atualmente você tem {} anos de idade, considerando o ano de 2024" .format(nascimento, idade))
+if nome.lower() == "fernando":
+    print("Você não tem acesso.")
+else:
+    print("Seja bem-vindo(a) {}".format(nome))
