525bef8311c6f6497e1add5ea20e0011b365e52f
Stofeles412 <fernandostofeles@gmail.com>
2024-09-23 14:14:13 -0300

    .

---
+++ curso-python/ex022.py
@@ -0,0 +1,5 @@
+nome = str (input("digite seu nome")).strip
+print("analisando seun nome...")
+print("seu nome em maiusculo é {}".format(nome.upper()))
+print("seu nome em minusculo é {}".format(nome.lower()))
+print("e seu nome possui {} letras".format(nome.__len__()))
---
+++ curso-python/ex024.py
@@ -0,0 +1,2 @@
+cid = str (input("digite em que cidade você nasceu :")).strip()
+print(cid[:5] == "santo")
