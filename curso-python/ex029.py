b1e32d96ec44e9f6b6efe2b3531fb413f1ffd499
Stofeles412 <fernandostofeles@gmail.com>
2024-09-29 10:49:31 -0300

    .

--- curso-python/ex029.py
+++ curso-python/ex029.py
@@ -1,10 +1,9 @@
 radar = float (input("qual velocidade vc passou ?"))
-vnormal = 80
-vmax = 100
-if radar <= 80:
-    print("vc estar na lei")
-
-else: print("vc ultarpassou a velcidade maxima permetida")
+multa = (radar - 80) * 7
+if radar > 80:
+    print("Multado vc exerdeu o limite de velecidade de 80km/h")
+    print("A multa foi no valor de de R${:.2f}".format(multa))
+print("Dirija sempre, com segurança")
 
 
     
---
+++ curso-python/ex030.py
@@ -0,0 +1,5 @@
+numero = int(input("digite um numero"))
+resultado = numero % 2
+if resultado == 0:
+    print("o numero {} Par".format(numero))
+else: print("O numero {} digitado é Impar".format(numero))
