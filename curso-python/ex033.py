44c43f686c69b1105e652de59b0d83b6aea610e1
Stofeles412 <fernandostofeles@gmail.com>
2024-09-30 14:15:01 -0300

    .

---
+++ curso-python/ex033.py
@@ -0,0 +1,7 @@
+from datetime import date
+ano = int (input("que ano quer analisar ? Coloque o 0, para analisar o ano atual")) 
+if ano == 0:
+    ano = date.today().year
+if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
+    print("o ano de {} é bissexto".format(ano))
+else:print("{} não é bissexto".format(ano))
---
+++ curso-python/ex034.py
@@ -0,0 +1,3 @@
+valor1 = int (input("digite o primeiro valor"))
+valor2 = int (input("digite o segundo valor"))
+valor3 = int (input("digite o terceiro valor"))
