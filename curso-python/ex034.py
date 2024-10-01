c9a6d8832d5cacd476a066e254b11893c521289c
Stofeles412 <fernandostofeles@gmail.com>
2024-10-01 18:43:00 -0300

    .

--- curso-python/ex034.py
+++ curso-python/ex034.py
@@ -1,3 +1,18 @@
-valor1 = int (input("digite o primeiro valor"))
-valor2 = int (input("digite o segundo valor"))
-valor3 = int (input("digite o terceiro valor"))
+a = int (input("digite o primeiro valor"))
+b = int (input("digite o segundo valor"))
+c = int (input("digite o terceiro valor"))
+menor = a
+if b<a and b<c:
+    menor = b
+if c<a and c<b:
+        menor = c
+
+
+maior = a
+if b>a and b>c:
+    maior = a
+if c>a and c>b:
+    maior = c 
+print("O menor valor é o {}".format(menor))
+print("o maior valor digitando foi p {}".format(maior))
+  
---
+++ curso-python/ex035.py
@@ -0,0 +1,4 @@
+sal = float (input("Qual o salrio do funcionario ?"))
+if sal <= 1250:
+    novo = salario + (15%)
+if sal >= salario + :
