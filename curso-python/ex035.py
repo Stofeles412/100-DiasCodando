3e26427ae32d4cd668f42f8da92c46c4918e168f
Stofeles412 <fernandostofeles@gmail.com>
2024-10-01 23:16:14 -0300

    Update ex035.py

--- curso-python/ex035.py
+++ curso-python/ex035.py
@@ -1,4 +1,6 @@
-sal = float (input("Qual o salrio do funcionario ?"))
+sal = float (input("Qual o salario do funcionario R$ ?"))
 if sal <= 1250:
-    novo = salario
-if novo >= (salario * 15 / 100 ):
+    novo =  sal + (sal * 15 / 100)
+else:
+    novo = sal + (sal * 10 / 100)
+print("O seu salario de {:.2f} com um aumento, você vai passar a ganhar R${:.2f}".format( sal, novo)) 
