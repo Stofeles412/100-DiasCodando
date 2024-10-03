2d70599748fb2772e4fba028f0e8a25be27f768d
Stofeles412 <fernandostofeles@gmail.com>
2024-10-03 20:38:17 -0300

    .

--- minha-marca/javascript.js
+++ minha-marca/javascript.js
@@ -1,4 +1,13 @@
 function Mostrar(){
-    var botao = document.getElementById()
+    var botao = document.getElementById("botao-projetos")
     var projetos = document.getElementById("art-projetos")
+    if(projetos.style.display == "none"){
+        projetos.style.display = "block"
+          botao.innerText = "voltar"
+      
+    }else{
+        projetos.style.display = "none"
+     
+      
+    }
 }
