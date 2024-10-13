0b8b9a0a64f9a1f0a2ebd057461c375abf84e4c6
Stofeles412 <fernandostofeles@gmail.com>
2024-10-12 21:59:37 -0300

    Update ex036.py

--- curso-python/ex036.py
+++ curso-python/ex036.py
@@ -2,4 +2,10 @@ valor = float(input("qual o valore do imovél desejado R$ ?"))
 sal = float (input("qual é seu salário mensal R$ ?"))
 anos = int(input("em até quantos anos você deseja pagar ?"))
 presta = valor / (anos * 12)
-print("valor {}".format(presta))
+minimo = sal * 30 / 100
+print("para pagar uma casa de {:.2f} em {} anos".format(valor, anos))
+print("A prestação será de {:.2f}R$ ".format(presta))
+if presta <= minimo:
+    print("o emprestimo pode ser concedido")
+else:
+    print("o emprestimo foi NEGADO !")
