a5b5acc31c114a8b8591e1de14d6fea3ef3385b4
Stofeles412 <fernandostofeles@gmail.com>
2024-11-10 11:52:31 -0300

    Update ex052.py

--- curso-python/ex052.py
+++ curso-python/ex052.py
@@ -1,3 +1,15 @@
 num = int(input("digite um número: "))
+tot = 0
 for c in range (1, num + 1):
-    print("{}".format(c), end= "")
+    if num % c == 0:
+        print("\033[32m ", end= " ")
+        tot += 1
+    else:
+        print("\033[31m ", end= " ")
+    print("{}".format(c), end= " ")
+print("\n\033[m0 O numero {} foi divísivel {} vez".format(num, tot))
+if tot == 2:
+    print("\033[32m Por isso ele é primo ! ")
+else:
+    print("\033[031m Por isso ele não é primo !")
+    
