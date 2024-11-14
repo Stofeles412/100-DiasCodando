4b069120180351cd9d849bc1d0da7a4961cf0d87
Stofeles412 <fernandostofeles@gmail.com>
2024-11-14 11:36:29 -0300

    Update index.py

--- curso-python/index.py
+++ curso-python/index.py
@@ -1,8 +1,3 @@
-n1 = float (input("digite uma nota"))
-n2 = float (input("digite outro numero"))
-m = (n1 + n2)/2
-print("a sua media foi {:.1f}".format(m))
-if m >= 6.0:
-   print("sua media foi boa parabens")
-else: 
-    print("sua media foi ruim :( estude mais !")
+for c in range (1,3):
+   n = int(input("digite um valor :"))
+print("fim")
