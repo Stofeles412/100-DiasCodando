8ef1891a4bd47d314b8b1e3de74fa6b57ebaff71
Stofeles412 <fernandostofeles@gmail.com>
2024-11-14 12:06:26 -0300

    Update index.py

--- curso-python/index.py
+++ curso-python/index.py
@@ -1,3 +1,10 @@
-for c in range (1,3):
-   n = int(input("digite um valor :"))
-print("fim")
+n = 1
+par = impar = 0
+while n != 0:
+   n = int(input("digite um valor: "))
+   if n != 0:
+      if n % 2 == 0:
+       par += 1
+      else:
+       impar += 1
+print("Vc digitou {} numeros pares e {} numeros impares : ".format(par, impar))
