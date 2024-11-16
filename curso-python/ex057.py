d57e9c910b56883775baeabacab6581704180c5c
Stofeles412 <fernandostofeles@gmail.com>
2024-11-15 23:35:05 -0300

    Update ex057.py

--- curso-python/ex057.py
+++ curso-python/ex057.py
@@ -1,4 +1,4 @@
 sexo = str(input("digite seu sexo [M/F] ")).strip().upper()[0]
 while sexo not in  "MF":
-      sexo = str(input("insira sexo validos M ou F")).strip().upper()[0]
+      sexo = str(input("Sexo inválido ! Tente novamente com F ou M")).strip().upper()[0]
 print("Sexo {} cadastrado com sucesso !".format(sexo))
