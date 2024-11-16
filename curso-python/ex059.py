863dd965956222b62af10095a0ed56bc0aebf4b8
Stofeles412 <fernandostofeles@gmail.com>
2024-11-16 11:33:58 -0300

    Update ex059.py

--- curso-python/ex059.py
+++ curso-python/ex059.py
@@ -1,3 +1,4 @@
+from time import sleep
 n1 = int(input("1° valor"))
 n2 = int(input("2° valor"))
 opçao  = 0
@@ -11,4 +12,24 @@ while opçao != 5:
      
          """)
     opçao = int(input("Escolha a opção : "))
-print("saindo do sistema...")
+    if opçao == 1:
+       soma = n1 + n2
+       print("A soma de {} + {} = {}".format(n1, n2, soma)) 
+    elif opçao == 2:
+        mult = n1 * n2
+        print("A multiplicação de {} x {} = {}".format(n1, n2, mult))
+    elif opçao == 3:
+        if n1 > n2:
+            print("O maior é {}".format(n1))
+        else:
+            print("O maior é o {}".format(n2))
+    elif opçao == 4:
+        n1 = int(input("1° valor"))
+        n2 = int(input("2° valor"))
+    else:
+
+     sleep (2)
+
+print("Finalizado com sucesso")
+
+
