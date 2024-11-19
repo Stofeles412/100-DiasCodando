54f8655185c98d630abe74d1fc3441afded7f362
Stofeles412 <fernandostofeles@gmail.com>
2024-11-18 22:24:18 -0300

    Update ex062.py

--- curso-python/ex062.py
+++ curso-python/ex062.py
@@ -13,6 +13,9 @@ while cont <= 10:
 while mais != 0:
     total = total + mais
     while cont <= total:
-     print("Pausa")
-     mais = int(input("Quantos termos a mais você deseja mostrar ?"))
-print("Fim")
+        print("{} >".format(termo), end=" ")
+        termo += rz
+        cont += 1
+    print("Pausa")
+    mais = int(input("Quantos termos a mais você deseja mostrar ?"))
+print("Progressão finalizada com {} termos ".format(total))
