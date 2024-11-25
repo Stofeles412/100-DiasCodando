7adb6fe340dbbaa638be890d43143f6902b24cd8
Stofeles412 <fernandostofeles@gmail.com>
2024-11-25 14:37:51 -0300

    atualizções de ux

--- minha-marca/javascript.js
+++ minha-marca/javascript.js
@@ -2,14 +2,14 @@ function Mostrar(){
     var botao = document.getElementById("botao-projetos")
     var projetos = document.getElementById("art-projetos")
     var corpo = document.getElementById("corpo")
-    if(projetos.style.display == "none"){
-        projetos.style.display = "block"
+    if(projetos.classList.contains("show")){
+        projetos.classList.remove ("show")
          botao.innerText = "Ocultar"
          botao.style.color = "blue"
       
       
     }else{
-        projetos.style.display = "none"
+        projetos.classList.add("show")
            botao.innerText = "Veja Mais"
         botao.style.color = "blueviolet"
      
@@ -17,22 +17,31 @@ function Mostrar(){
     }
 }
 
-function mudarTema(){
-    const fundo = document.getElementById ("containe-principal")
-    botaoTema = document.getElementById ("botao-tema")
-    const icone = document.getElementById("icone-tema")
-    if (fundo.style.background === "white"){
-        fundo.style.background = "black"
-        corpo.style.background = "gray"
-        botaoTema.style.background = "#4d0897"
-        fundo.style.color = "darkgray"
-          icone.src = "../imagens/brilho-do-sol.png"
-      
-    }else{
-        fundo.style.background = "white"
-        fundo.style.color = "black"
-        corpo.style.background = "white"
-          botaoTema.style.background = "#721ccd"
-         icone.src = "../imagens/lua-crescente.png"
+function mudarTema() {
+    const corpo = document.getElementById("corpo");
+    const fundo = document.getElementById("containe-principal");
+    const botaoTema = document.getElementById("botao-tema");
+    const icone = document.getElementById("icone-tema");
+
+    if (corpo.classList.contains("tema-claro")) {
+        // Alterar para tema escuro
+        corpo.classList.remove("tema-claro");
+        corpo.classList.add("tema-escuro");
+
+        fundo.classList.add("fundo-escuro");
+        fundo.classList.remove("fundo-claro");
+
+        botaoTema.style.background = "#4d0897";
+        icone.src = "../imagens/brilho-do-sol.png";
+    } else {
+        // Alterar para tema claro
+        corpo.classList.remove("tema-escuro");
+        corpo.classList.add("tema-claro");
+
+        fundo.classList.add("fundo-claro");
+        fundo.classList.remove("fundo-escuro");
+
+        botaoTema.style.background = "#721ccd";
+        icone.src = "../imagens/lua-crescente.png";
     }
 }
