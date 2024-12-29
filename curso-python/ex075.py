8bedbb63ecf51c78e6f20973226013a2a51db0bb
Stofeles412 <fernandostofeles@gmail.com>
2024-12-29 19:14:06 -0300

    .

---
+++ curso-python/ex075.py
@@ -0,0 +1,3 @@
+numero = (int(input("Digite um numero: ")),int(input("Digite outro numero: ")),int(input("Digite mais um numero: ")),int(input("Digite o ultimo numero: ")))
+print(f"Vc digitou os valores{numero}")
+print(f"O valor 9 apareceu {min(numero)
