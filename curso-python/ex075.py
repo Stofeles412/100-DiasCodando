201f1bb9256c9313d5559eb9ea3f93bdd8acd327
Stofeles412 <fernandostofeles@gmail.com>
2024-12-29 19:34:05 -0300

    .

--- curso-python/ex075.py
+++ curso-python/ex075.py
@@ -1,3 +1,12 @@
 numero = (int(input("Digite um numero: ")),int(input("Digite outro numero: ")),int(input("Digite mais um numero: ")),int(input("Digite o ultimo numero: ")))
 print(f"Vc digitou os valores{numero}")
-print(f"O valor 9 apareceu {min(numero)
+print(f"O valor 9 apareceu {numero.count(9)} vezes ")
+if 3 in numero:
+    print(f"O valor 3 aparceu na {numero.index(3)+1}ª posição ")
+else:
+    print("O valor 3 não foi encontado em nehuma posição")
+print(f"Os valores pares digitados fora {n}", end= " ")
+if n in numero:
+    if n % 2 == 0:
+        print(n, end=" ")
+        
