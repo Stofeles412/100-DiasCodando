585d777e368045dcdc1df707d37a08b0f4dee1e1
Stofeles412 <fernandostofeles@gmail.com>
2025-01-03 18:44:11 -0300

    Update meus.py

--- curso-python/meus.py
+++ curso-python/meus.py
@@ -1,3 +1,23 @@
-cadastro = list()
+cadastro = []
+
 while True:
-    nome = str
+    nome = input("Qual o nome que quer cadastrar? ").strip()
+    
+    if nome not in cadastro:
+        cadastro.append(nome)
+        print("Nome cadastrado com sucesso!")
+    else:
+        print("Nome já cadastrado.")
+    
+    while True:
+        mais = input("Deseja continuar? [S/N]: ").strip().upper()
+        if mais in ["S", "N"]:
+            break
+        print("Resposta inválida. Digite 'S' para continuar ou 'N' para sair.")
+    
+    if mais == "N":
+        print("Fim")
+        break
+
+print(f"Os nomes {cadastro} foram cadastrados com sucesso.")
+
