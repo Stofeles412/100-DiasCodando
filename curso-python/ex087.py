d9c4091706cc0b5deb11b96028cc1261643e3ea6
Stofeles412 <fernandostofeles@gmail.com>
2025-01-14 20:59:39 -0300

    .

--- curso-python/ex087.py
+++ curso-python/ex087.py
@@ -1,22 +1,28 @@
 from random import randint
+
 lista = []
 jogos = []
-print('=-'* 30)
+print('=-' * 30)
 print("JOGUE NA MEGA SENA")
-print('=-'* 30)
-quant = int(input("Quantos jogos você deseja sortear: ?"))
-tot = 0
+print('=-' * 30)
+quant = int(input("Quantos jogos você deseja sortear? "))
+tot = 1
+
 while tot <= quant:
     cont = 0
     while True:
-      numero = randint(1,60)
-      if numero  not in lista:
-         lista.append(numero)
-         cont += 1
-      if cont >= 6:
-        break 
-lista.sort()
-jogos.append(lista[:])
-lista.clear()
-print(f"As jogadas ficaram da seguinte forma {jogos}")
+        numero = randint(1, 60)
+        if numero not in lista:
+            lista.append(numero)
+            cont += 1
+        if cont >= 6:
+            break
+    lista.sort()
+    jogos.append(lista[:])  
+    lista.clear() 
+    tot += 1
 
+print('=-' * 30)
+print("As jogadas ficaram da seguinte forma:")
+for i, jogo in enumerate(jogos, 1):
+    print(f"Jogo {i}: {jogo}")
