94ca4d07155895f67ad6c73ac76c53c5f570aa5c
Stofeles412 <fernandostofeles@gmail.com>
2025-01-20 14:35:26 -0300

    Create ex095.py

---
+++ curso-python/ex095.py
@@ -0,0 +1,9 @@
+dados = dict()
+while True:
+    dados["nome"] = str(input("Digite seu nome: ? "))
+    dados["idade"] = int(input("Digite sua idade: "))
+    dados["Estado"] = str(input("Qual estado você reside: ?"))
+    res = str(input("Deseja continuar: ? [S/N]")).upper().strip()
+    if res == "N":
+        break
+print(dados)
