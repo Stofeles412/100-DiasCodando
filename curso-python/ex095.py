ac9536bc2668eda2d8b3c29ba5ab5002976c6065
Stofeles412 <fernandostofeles@gmail.com>
2025-01-21 14:07:13 -0300

    Update ex095.py

--- curso-python/ex095.py
+++ curso-python/ex095.py
@@ -8,26 +8,41 @@ while True:
     tot = int(input(f'Quantas partidas {dados["nome"]} jogou: ?'))
     partidas.clear()
     for c in range(0, tot):
-        partidas.append(int (input(f"Quantos gols na partida {c}° ?" )))
+        partidas.append(int (input(f"Quantos gols na partida {c +1}° ?" )))
     dados["gols"] = partidas[:]
     dados["total"] = sum(partidas)
     time.append(dados.copy())
     while True:
-        res = str(input("Deseja continuar ? [S/N] ")).upper()[0]
+        res = str(input("Deseja continuar ? [S/N] ")).upper()[0].strip()
         if res in "SN":
          break
         print("ERRO !! Responda com apenas S - N ")
-    
     if res == "N":
         break
+print("-="* 30)
+print("cod ", end="")
+for i in dados.keys():
+    print(f"{i:<15}", end="")
+print()   
 print("_"* 30)
 for k, v in enumerate(time):
     print(f"{k:>4}", end=" ")
     for d in v.values():
         print(f"{str(d):<15}", end="")
     print()
-print("-="*30)
-        
+print("-="*40)
+while True:
+    busca = int(input("Qual jogador deseja mostrar (999 para parar)" ))
+    if busca == 999:
+        break
+    if busca >= len(time):
+        print(f"Erro! não existe jogador com o codigo {busca}")   
+    else:
+        print(f"levantamento do jogador {time[busca]['nome']}:")  
+        for i, g in enumerate(time[busca]['gols']):
+            print(f"  No jogo {i} fez {g} gols")
+        print("_"* 30)   
+print("<<< Volta sempre :) <<<")   
     
     
 
