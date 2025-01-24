b4337e4da70aed153f817fd56fbf9614cf16d40d
Stofeles412 <fernandostofeles@gmail.com>
2025-01-23 21:11:05 -0300

    .

---
+++ curso-python/aula020.py
@@ -0,0 +1,6 @@
+def soma (larg, compri):
+    res = larg * compri
+    print(f"Soma do terreno {larg} x {compri} = {res}m")
+compri = float(input("Comprimento cm"))
+larg = float(input("largura cm"))
+soma(compri,larg)
---
+++ curso-python/ex096.py
@@ -0,0 +1,13 @@
+def soma(larg, compri):
+    res = larg * compri
+    print(f"Aréa do terreno de {larg} x {compri} = {res}m")
+print("-="*30)
+larg = float(input("Qual a largura cm do seu terreno: ?"))
+compri = float(input("Qual o comprimento cm do seu terreno: ?"))
+soma(larg, compri)
+
+
+
+    
+    
+    
