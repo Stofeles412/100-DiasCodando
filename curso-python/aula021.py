9ccbca228e95e427e6b7e88b3dc7989a4e037343
Stofeles412 <fernandostofeles@gmail.com>
2025-01-28 11:41:21 -0300

    Update aula021.py

--- curso-python/aula021.py
+++ curso-python/aula021.py
@@ -1,12 +1,19 @@
-def contador(i,f,p):
-    c = i
-    while c <= f:
-        print(f"{c}", end="")
-        c += p
-        print("Fim")
-    contador(2,10,2)
-help(contador)
 
-
+#def somar(a=3,b=7,c=10):
+    #s = a + b + c
+   # print(f"A soma vale {s}")
     
+    
+#somar()
+
+def teste(a):
+    a= 3 
+    b += 4
+    c = 2
+    print(f"A: dentro vale {a}")
+    print(f"B: dentro vale {b}")
+    print(f"C: dentro vale {c}")
+a = 5
+teste()
+print(f"A fora vale {a}")
 
