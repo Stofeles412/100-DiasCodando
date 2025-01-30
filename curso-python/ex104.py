18660da8c8eb82a98eb33f6bf7604db77d293c20
Stofeles412 <fernandostofeles@gmail.com>
2025-01-30 12:42:25 -0300

    Create ex104.py

---
+++ curso-python/ex104.py
@@ -0,0 +1,15 @@
+def leiaint(msg):
+    ok = False
+    while True:
+      n = str(input(msg))
+      if n.isnumeric():
+            valor = int(n)
+            ok = True
+      else:
+             print("\033[0;31m erro! prfv digite valores validos. \033[m")
+      if ok:
+              break
+    return valor
+
+n = leiaint("Digite um numero: ")
+print(f"Vc acabou de digitar o numero {n}")
