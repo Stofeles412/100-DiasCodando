5c2ecd738323784bc5ab5dcda33b398e2e2852e4
Stofeles412 <fernandostofeles@gmail.com>
2025-02-12 18:50:28 -0300

    Update ex113.py

--- curso-python/ex113.py
+++ curso-python/ex113.py
@@ -5,6 +5,9 @@ def leiaint(msg):
     except(ValueError, TypeError):
       print(' \033[31m Erro ! Por favor digite um valor válido\033[m')
       continue
+    except(KeyboardInterrupt):
+      print('\n\033[31m usuario não digitou valor\033[m')
+      return 0
     else:
       return n
     
@@ -12,3 +15,23 @@ def leiaint(msg):
       
 num = leiaint("Digite um valor:")
 print(f'O valor digitado foi {num}')
+
+
+def leiafloat(msg):
+  while True:
+    try:
+      n = float(input(msg))
+    except(ValueError, TypeError):
+      print(' \033[31m Erro ! Por favor digite um valor válido\033[m')
+      continue
+    except(KeyboardInterrupt):
+      print('\n\033[31m usuario não digitou valor\033[m')
+      return 0
+    else:
+      return n
+    
+    
+      
+num = leiafloat("Digite um valor:")
+n2 = leiafloat("Digite um valor float")
+print(f'O valor digitado foi {num} e o float fou {n2}')
